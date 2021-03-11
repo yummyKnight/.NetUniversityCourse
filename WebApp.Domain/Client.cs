bb98b41d35ff65669274528193e67d3b26689281
Nick <sshedl22@gmail.com>
2021-03-11 12:14:52 +0300

    Quick adjustment in Client model

--- WebApp.Domain/Client.cs
+++ WebApp.Domain/Client.cs
@@ -9,5 +9,6 @@ namespace WebApp.Domain {
         public uint Id { get; }
         public string FullName { get; set; }
         public string Address { get; set; }
+        public uint OrdersNum { get; set; }
     }
 }
